487360c16a94598f2eecc8d6362ae806b76e8384
root <3166360451@qq.com>
2025-02-02 20:01:33 +0800

    4.6.1

--- app/src/main/java/com/aliangmaker/media/fragment/MoreUpLogFragment.java
+++ app/src/main/java/com/aliangmaker/media/fragment/MoreUpLogFragment.java
@@ -1,19 +1,23 @@
 package com.aliangmaker.media.fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Toast;
 import androidx.fragment.app.Fragment;
 
 import com.aliangmaker.media.R;
 
 public class MoreUpLogFragment extends Fragment {
-
+    SharedPreferences sharedPreferences;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        sharedPreferences = getContext().getSharedPreferences("main", Context.MODE_PRIVATE);
         return inflater.inflate(R.layout.fragment_more_up_log, container, false);
     }
 
@@ -27,7 +31,10 @@ public class MoreUpLogFragment extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
-        TitleFragment.setTitle("凉腕播放器");
+        if (!sharedPreferences.getString("version","3.14.10").equals(getString(R.string.version))) {
+            sharedPreferences.edit().putString("version",getString(R.string.version)).apply();
+            TitleFragment.setTitle("赞助我们");
+        } else TitleFragment.setTitle("凉腕播放器");
         TitleFragment.setBackGone(true);
     }
 }
